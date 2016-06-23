c906f2f773c531857063f15579b0d6ef21d70bec
Jirayus Jiarpakdee <jiyoyo@hotmail.com>
2016-06-23 20:45:55 +0900

    Optimize by compile function before execute

--- collinearityDefectData.R
+++ collinearityDefectData.R
@@ -10,64 +10,143 @@ setwd(script.dir)
 #install_github("klainfo/DefectData")
 library(DefectData)
 library(Hmisc)
+library(compiler)
+
+enableJIT(3)
+
+## Global variables
+failVarclus <- c()
+failCarVIF <- c()
+failRMSVIF <- c()
+datasetInfo <- c()
 
 ## Define functions
-writeLine <- function(arg1,arg2){
+.writeLine <- function(arg1,arg2){
   write(paste0(arg2,collapse=","),file=arg1,append=TRUE)  
-}
+} 
+writeLine <- cmpfun(.writeLine)
 
-corrGraph <- function(dataset,refOrder,dataHeader){
+.exportDataDist <- function(dataset,dataHeader){
+  # plot dataset distribution
+  print("Plot and export dataDist")
+  
+  png(height=6000, width=6000, pointsize=15, file=paste0("./output/datasetDist/",dataHeader,"_dataDist.png"))
+  pairs(dataset, upper.panel = panel.cor)
+  dev.off()
+}
+exportDataDist <- cmpfun(.exportDataDist)
+
+.corrGraph <- function(dataset,refOrder,dataHeader){
   
   vc <- varclus(~.,
                 data=dataset[refOrder],
                 similarity = "spearman",
                 trans="abs")
   png(height=1080, width=1080, pointsize=15, file=paste0("./output/varclus/",dataHeader,"_varclus.png"))
+  
+  # TRUE if there are variables that have more than 0.7 similarity
+  datasetInfo <<- c(datasetInfo,Reduce("|",(1-vc$hclust$height)>=0.7))
+  
   plot(vc)
   abline(h=0.3) # threashold 0.7 on graph
   dev.off()
+  
 }
+corrGraph <- cmpfun(.corrGraph)
 
-getVIF <- function(dataset,refOrder,depVar,dataHeader){
+.getVIF <- function(dataset,refOrder,depVar,dataHeader,dataID){
+  
   model <- glm(as.formula(paste(
     depVar, "~", paste(refOrder, collapse = '+')
   )),
   data = dataset,
   family = binomial())
-  model.vif <- vif(model)[refOrder]
-  # Header, n(HighlyCorrelatedVar) Text,Indep
-  # Header, n(HighlyCorrelatedVar),VIF
-  writeLine("./output/VIF.csv",paste0(c(dataHeader,"n(highCol)",names(model.vif)),collapse=","))
-  writeLine("./output/VIF.csv",paste0(c(dataHeader,length(model.vif[model.vif>5]),model.vif),collapse=","))
+  
+  # Header, Package, n(HighCol),Indep
+  writeLine("./output/VIF.csv",paste0(c(dataHeader,"Package","n(HighCol)",refOrder),collapse=","))
+  
+  # Header, car,car::n(HighlyCorrelatedVar),car::VIF
+  tryCatch({
+    model.vif <- car::vif(model)[refOrder]
+    writeLine("./output/VIF.csv",paste0(c(dataHeader,"car",length(model.vif[model.vif>5]),model.vif),collapse=","))
+  },
+  error = function(e){
+    print(paste0("Fail to export car::VIF on ",dataHeader))
+    failCarVIF <<- c(failCarVIF,dataID)
+  })
+  
+  # Header, rms,rms::n(HighlyCorrelatedVar),rms::VIF
+  tryCatch({
+    model.vif <- rms::vif(model)[refOrder]
+    writeLine("./output/VIF.csv",paste0(c(dataHeader,"rms",length(model.vif[model.vif>5]),model.vif),collapse=","))
+    
+    # Check if there are any variables that have vif value more than 5 and 10 (Collinearity)
+    datasetInfo <<- c(datasetInfo,Reduce("|",model.vif>=5))
+    datasetInfo <<- c(datasetInfo,Reduce("|",model.vif>=10))
+    
+  },
+  error = function(e){
+    print(paste0("Fail to export rms::VIF on ",dataHeader))
+    failRMSVIF <<- c(failRMSVIF,dataID)
+    datasetInfo <<- c(datasetInfo,"FAIL","FAIL")
+  })
 }
+getVIF <- cmpfun(.getVIF)
+
+# Take from http://www.r-bloggers.com/scatter-plot-matrices-in-r/
+.panel.cor <- function(x, y, digits = 2, cex.cor, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  # correlation coefficient
+  r <- cor(x, y)
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste("r= ", txt, sep = "")
+  text(0.5, 0.6, txt)
+  
+  # p-value calculation
+  p <- cor.test(x, y)$p.value
+  txt2 <- format(c(p, 0.123456789), digits = digits)[1]
+  txt2 <- paste("p= ", txt2, sep = "")
+  if(p<0.01) txt2 <- paste("p= ", "<0.01", sep = "")
+  text(0.5, 0.4, txt2)
+}
+panel.cor <- cmpfun(.panel.cor)
+
 
 ## Main
 
 # Create output folder 
 dir.create(file.path(paste0(getwd(), '/output/')), showWarnings = FALSE)
 dir.create(file.path(paste0(getwd(), '/output/varclus/')), showWarnings = FALSE)
-
-failVarclus <- c()
-failVIF <- c()
+dir.create(file.path(paste0(getwd(), '/output/datasetDist/')), showWarnings = FALSE)
+writeLine("./defectData_summarize.csv",paste0(c("DataId","Name","Varclus-0.7","VIF(rms)-5","VIF(rms)-10"),collapse=","))
 
 for(targetProjectId in 1:nrow(listData)) {
 
-  print(paste('Start ProjectID:', targetProjectId))
+  print(paste('ProjectID:', targetProjectId,"START"))
+  datasetInfo <<- c(targetProjectId)  
   
   targetData <- as.character(listData[targetProjectId, 1])
+  datasetInfo <<- c(datasetInfo,targetData)
   
-  ## Load data
+  # Load data
   Data <- loadData(targetData)
+  
   # retrieve data, name of dependent and independent variables
   dataset <- Data$data
   dep <- Data$dep
   indep <- Data$indep
+  
+  # Export dataDist of dataset
+  exportDataDist(dataset[indep],targetData)
+  
   # convert fron logical -> factor variable type
   dataset <- dataset[c(indep, dep)]
   dataset[dep] <-
     lapply(dataset[dep], function(x)
       factor(ifelse(x, "true", "false")))
   
+  print("Plot and export varclus")
   tryCatch(
     {
       corrGraph(dataset,indep,targetData)
@@ -75,19 +154,22 @@ for(targetProjectId in 1:nrow(listData)) {
     error = function(e){
       print(paste0("Fail to export correlation graph(varclus) on ",targetData))
       failVarclus <<- c(failVarclus,targetProjectId)
+      datasetInfo <<- c(datasetInfo,"FAIL")
       })
   
-  tryCatch(
-    {
-      getVIF(dataset,indep,dep,targetData)
-    },
-    error = function(e){
-      print(paste0("Fail to export VIF on ",targetData))
-      failVIF <<- c(failVIF,targetProjectId)
-    })
+  print("Build model and compute VIF")
+  getVIF(dataset,indep,dep,targetData,targetProjectId)
+    
+  writeLine("./defectData_summarize.csv",datasetInfo)
+  
+  print(paste('ProjectID:', targetProjectId,"DONE"))
 }
 
-print("Fail to varclus projectID")
+print(paste0("Fail to varclus(",length(failVarclus),") projectID"))
 print(failVarclus)
-print("Fail to VIF projectID")
-print(failVIF)
+print(paste0("Fail to car::VIF(",length(failCarVIF),") projectID"))
+print(failCarVIF)
+print(paste0("Fail to rms::VIF(",length(failRMSVIF),") projectID"))
+print(failRMSVIF)
+
+
